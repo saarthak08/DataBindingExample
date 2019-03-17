c607f2f48b1b7feaca34f64d4683903791fb18c1
Saarthak <saarthakgupta08@gmail.com>
2019-03-17 18:52:20 +0530

    Added TwoWayBinding

--- app/src/main/java/com/example/databindingexample/IncludeLayoutActivity.java
+++ app/src/main/java/com/example/databindingexample/IncludeLayoutActivity.java
@@ -7,6 +7,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.WindowManager;
 
 import com.example.databindingexample.databinding.ActivityIncludeLayoutBinding;
 
--- app/src/main/java/com/example/databindingexample/Student.java
+++ app/src/main/java/com/example/databindingexample/Student.java
@@ -1,6 +1,9 @@
 package com.example.databindingexample;
 
-public class Student {
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
+public class Student extends BaseObservable {
 
     public Student(String studentName, String studentEmail) {
         this.studentName = studentName;
@@ -13,19 +16,23 @@ public class Student {
     private String studentName;
     private String studentEmail;
 
+    @Bindable
     public String getStudentName() {
         return studentName;
     }
 
     public void setStudentName(String studentName) {
         this.studentName = studentName;
+        notifyPropertyChanged(BR.studentName);
     }
 
+    @Bindable
     public String getStudentEmail() {
         return studentEmail;
     }
 
     public void setStudentEmail(String studentEmail) {
         this.studentEmail = studentEmail;
+        notifyPropertyChanged(BR.studentEmail);
     }
 }
